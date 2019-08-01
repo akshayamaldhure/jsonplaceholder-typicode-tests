80a5a6cd22529beaf25087e753cf638148dcecf5
Akshay Maldhure <akshaymaldhure@gmail.com>
2019-08-02 00:18:26 +0530

    Refactor environment.json under resources

--- src/main/java/config/ConfigProvider.java
+++ src/main/java/config/ConfigProvider.java
@@ -9,7 +9,7 @@ public class ConfigProvider {
     public static Object getConfigObject(String testEnvironment) {
         String projectDir = System.getProperty("user.dir");
         String pathSep = File.separator;
-        String configFilePath = projectDir + pathSep + "src" + pathSep + "main" + pathSep + "java" + pathSep + "config" + pathSep + "environment.json";
+        String configFilePath = projectDir + pathSep + "src" + pathSep + "main" + pathSep + "resources" + pathSep + "environment.json";
         System.out.println("Config file: " + configFilePath);
         System.out.println("Test environment: " + testEnvironment);
         return FileUtils.getJsonFileAsJSONObject(configFilePath).get(testEnvironment);
