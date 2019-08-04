120345119239591bfc4bd486335ede41d4834313
Akshay Maldhure <akshaymaldhure@gmail.com>
2019-08-04 11:28:30 +0530

    Refactor environment.json -> environments.json

--- src/main/java/config/ConfigProvider.java
+++ src/main/java/config/ConfigProvider.java
@@ -8,12 +8,11 @@ import java.io.IOException;
 
 public class ConfigProvider {
 
-    public static Object getConfigObject(String testEnvironment) throws IOException, ParseException {
+    public static Object getConfigObject(String configKey) throws IOException, ParseException {
         String projectDir = System.getProperty("user.dir");
         String pathSep = File.separator;
-        String configFilePath = projectDir + pathSep + "src" + pathSep + "main" + pathSep + "resources" + pathSep + "environment.json";
-        System.out.println("Config file: " + configFilePath);
-        System.out.println("Test environment: " + testEnvironment);
-        return FileUtils.getJsonFileAsJSONObject(configFilePath).get(testEnvironment);
+        String configFilePath = projectDir + pathSep + "src" + pathSep + "main" + pathSep + "resources" + pathSep + "environments.json";
+        System.out.println("Configuration key: " + configKey);
+        return FileUtils.getJsonFileAsJSONObject(configFilePath).get(configKey);
     }
 }
