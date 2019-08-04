5ff765e4dd0d17d4cd1d531d473e8b8baf333a27
Akshay Maldhure <akshaymaldhure@gmail.com>
2019-08-04 22:34:42 +0530

    Refactor: Use component methods to make API calls

--- src/test/java/tests/PostTests.java
+++ src/test/java/tests/PostTests.java
@@ -1,59 +1,32 @@
 package tests;
 
 import com.jayway.jsonpath.JsonPath;
-import config.Environment;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
+import components.CommentComponent;
+import components.PostComponent;
+import components.UserComponent;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
 import models.Comment;
 import models.Post;
 import org.junit.Assert;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.greaterThan;
 
 public class PostTests {
 
-    RequestSpecification requestSpec;
-    ResponseSpecification responseSpec;
-    Environment environment;
-    Response allUsersResponse;
-    Response allPostsByUserResponse;
-    Response allCommentsOnUserPostsResponse;
-    int userId;
-    List<Post> allPostsByUser;
-    List<Comment> allCommentsOnUserPosts;
-
-    @BeforeSuite
-    public void setupRequestResponseSpecs() {
-        environment = new Environment();
-        System.out.println("Setting up the request and response specifications");
-        requestSpec = new RequestSpecBuilder()
-                .setBaseUri(environment.baseUrl)
-                .addHeader("Content-Type", "application/json")
-                .build();
-        responseSpec = new ResponseSpecBuilder()
-                .expectStatusCode(200)
-                .build();
-    }
+    private Response allUsersResponse;
+    private Response allCommentsOnUserPostsResponse;
+    private int userId;
+    private List<Post> allPostsByUser;
+    private  List<Comment> allCommentsOnUserPosts;
 
     @Test
     public void verifyGetAllUsers() {
-        allUsersResponse = given()
-                .spec(requestSpec)
-                .get(environment.usersEndpoint);
-        allUsersResponse
-                .then()
-                .assertThat()
-                .spec(responseSpec);
+        allUsersResponse = UserComponent.getAllUsers();
         allUsersResponse
                 .then()
                 .assertThat()
@@ -73,15 +46,7 @@ public class PostTests {
 
     @Test(dependsOnMethods = "verifyGetUserIdForUser")
     public void verifyGetPostsByUserId() {
-        allPostsByUserResponse = given()
-                .spec(requestSpec)
-                .param("userId", userId)
-                .get(environment.postsEndpoint);
-        allPostsByUserResponse
-                .then()
-                .assertThat()
-                .spec(responseSpec);
-        allPostsByUser = Arrays.asList(allPostsByUserResponse.getBody().as(Post[].class));
+        allPostsByUser = Arrays.asList(PostComponent.getPosts(userId).getBody().as(Post[].class));
         allPostsByUser.forEach(post -> Assert.assertEquals(post.getUserId(), userId));
     }
 
@@ -89,14 +54,7 @@ public class PostTests {
     public void verifyPostComments() {
         allPostsByUser.forEach(post -> {
             int postId = post.getId();
-            allCommentsOnUserPostsResponse = given()
-                    .spec(requestSpec)
-                    .param("postId", postId)
-                    .get(environment.commentsEndpoint);
-            allCommentsOnUserPostsResponse
-                    .then()
-                    .assertThat()
-                    .spec(responseSpec);
+            allCommentsOnUserPostsResponse = CommentComponent.getCommentsOnPost(postId);
             allCommentsOnUserPosts = Arrays.asList(allCommentsOnUserPostsResponse.getBody().as(Comment[].class));
             String emailRegex = "^(.+)@(.+)$";
             allCommentsOnUserPosts.forEach(comment -> {
