267324672ef6f3f7a824978cd68986077a85163a
Akshay Maldhure <akshaymaldhure@gmail.com>
2019-08-04 22:31:26 +0530

    Refactor: Pass requestSpec to the API call

--- src/main/java/common/CommonTests.java
+++ src/main/java/common/CommonTests.java
@@ -1,13 +1,13 @@
 package common;
 
-import io.restassured.response.Response;
-
 import static io.restassured.RestAssured.given;
 
 public class CommonTests {
 
     public static boolean isAppBackendUp() {
-        Response response = given().get();
-        return response.statusCode() == 200;
+        return given()
+                .spec(Spec.requestSpec)
+                .get().statusCode() == 200;
+
     }
 }
