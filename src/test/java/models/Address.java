6459246843fdcf258fe0c3e0dc6dc5858c7852bb
Akshay Maldhure <akshaymaldhure@gmail.com>
2019-08-05 16:25:24 +0530

    Add POJO classes that describe a user and it's sub-properties

---
+++ src/test/java/models/Address.java
@@ -0,0 +1,91 @@
+package models;
+
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "street",
+        "suite",
+        "city",
+        "zipcode",
+        "geo"
+})
+public class Address { // POJO class that describes a user's address
+
+    @JsonProperty("street")
+    private String street;
+    @JsonProperty("suite")
+    private String suite;
+    @JsonProperty("city")
+    private String city;
+    @JsonProperty("zipcode")
+    private String zipcode;
+    @JsonProperty("geo")
+    private Geo geo;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("street")
+    public String getStreet() {
+        return street;
+    }
+
+    @JsonProperty("street")
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    @JsonProperty("suite")
+    public String getSuite() {
+        return suite;
+    }
+
+    @JsonProperty("suite")
+    public void setSuite(String suite) {
+        this.suite = suite;
+    }
+
+    @JsonProperty("city")
+    public String getCity() {
+        return city;
+    }
+
+    @JsonProperty("city")
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @JsonProperty("zipcode")
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    @JsonProperty("zipcode")
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @JsonProperty("geo")
+    public Geo getGeo() {
+        return geo;
+    }
+
+    @JsonProperty("geo")
+    public void setGeo(Geo geo) {
+        this.geo = geo;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ src/test/java/models/Geo.java
@@ -0,0 +1,52 @@
+package models;
+
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "lat",
+        "lng"
+})
+public class Geo { // POJO class that describes a user's geo location
+
+    @JsonProperty("lat")
+    private String lat;
+    @JsonProperty("lng")
+    private String lng;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("lat")
+    public String getLat() {
+        return lat;
+    }
+
+    @JsonProperty("lat")
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    @JsonProperty("lng")
+    public String getLng() {
+        return lng;
+    }
+
+    @JsonProperty("lng")
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
